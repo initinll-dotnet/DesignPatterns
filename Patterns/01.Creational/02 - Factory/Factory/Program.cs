3fc96b24430d9f519ab47e59069ca210b7c407ff
initinll <initinli@yahoo.com>
2024-11-29 21:55:54 +0530

    added Factory pattern concepts

---
+++ Patterns/01.Creational/02 - Factory/Factory/Program.cs
@@ -0,0 +1,20 @@
+﻿using Factory;
+
+Console.Title = "Factory Method";
+
+List<DiscountFactory> factories = 
+[
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("BE") 
+];
+
+foreach (var factory in factories)
+{
+    var discountService = factory.CreateDiscountService();
+
+    var log = $"Percentage {discountService.DiscountPercentage} coming from {discountService}";
+
+    Console.WriteLine(log);
+}
+
+Console.ReadKey();
