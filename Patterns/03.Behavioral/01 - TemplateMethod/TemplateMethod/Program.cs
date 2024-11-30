382707191c1d83256d308d51f7c387cf3b9c7000
initinll <initinli@yahoo.com>
2024-11-30 19:53:12 +0530

    added TemplateMethod pattern concept

---
+++ Patterns/03.Behavioral/01 - TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+ExchangeMailParser exchangeMailParser = new();
+Console.WriteLine(exchangeMailParser.ParseMailBody(identifier: "111-exchange-111"));
+Console.WriteLine();
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody(identifier: "222-apache-222"));
+Console.WriteLine();
+
+EudoraMailParser eudoraMailParser = new();
+Console.WriteLine(eudoraMailParser.ParseMailBody(identifier: "333-eudora-333"));
+
+Console.ReadKey();
