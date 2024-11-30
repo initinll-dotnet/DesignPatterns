2e52ecd61195408978e7568f9e5871db34ec8e4e
initinll <initinli@yahoo.com>
2024-11-30 20:41:38 +0530

    added strategy pattern

---
+++ Patterns/03.Behavioral/02 - Strategy/Strategy/Implementation.cs
@@ -0,0 +1,83 @@
+﻿namespace Strategy;
+
+/// <summary>
+/// Strategy
+/// </summary>
+public interface IExportService
+{
+    void Export(Order order);
+}
+
+/// <summary>
+/// ConcreteStrategy
+/// </summary>
+public class JsonExportService : IExportService
+{
+    public void Export(Order order)
+    {
+        Console.WriteLine($"Exporting {order.Name} to Json.");
+    }
+}
+
+/// <summary>
+/// ConcreteStrategy
+/// </summary>
+public class XMLExportService : IExportService
+{
+    public void Export(Order order)
+    {
+        Console.WriteLine($"Exporting {order.Name} to XML.");
+    }
+}
+
+/// <summary>
+/// ConcreteStrategy
+/// </summary>
+public class CSVExportService : IExportService
+{
+    public void Export(Order order)
+    {
+        Console.WriteLine($"Exporting {order.Name} to CSV.");
+    }
+}
+
+/// <summary>
+/// Context
+/// </summary>
+public class Order
+{
+    private readonly IExportService _exportService;
+
+    public Order(
+        string customer,
+        int amount,
+        string name,
+        IExportService exportService)
+    {
+        Customer = customer;
+        Amount = amount;
+        Name = name;
+        _exportService = exportService;
+    }
+    public string Customer { get; set; }
+    public int Amount { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+
+    // Variation 1
+    public void Export1()
+    {
+        _exportService.Export(this);
+    }
+
+    // Variation 2
+    public void Export2(IExportService exportService)
+    {
+        if (exportService is null)
+        {
+            throw new ArgumentNullException(nameof(exportService));
+        }
+
+        exportService.Export(this);
+    }
+}
---
+++ Patterns/03.Behavioral/02 - Strategy/Strategy/Program.cs
@@ -0,0 +1,25 @@
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+var order = new Order(
+    customer: "Marvin Software", 
+    amount: 5, 
+    name: "Visual Studio License", 
+    exportService: new CSVExportService());
+// Variation 1
+order.Export1();
+// Variation 2
+order.Export2(new CSVExportService());
+
+order = new Order(
+    customer: "Marvin Software",
+    amount: 5,
+    name: "Visual Studio License",
+    exportService: new JsonExportService());
+// Variation 1
+order.Export1();
+// Variation 2
+order.Export2(new JsonExportService());
+
+Console.ReadKey();
