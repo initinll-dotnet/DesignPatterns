33157d8deac38d722436ab4e52ac684a6f08f101
initinll <initinli@yahoo.com>
2024-12-01 14:40:38 +0530

    added Mediator pattern concept

---
+++ Patterns/03.Behavioral/05 - Mediator/Mediator/Implementation.cs
@@ -0,0 +1,132 @@
+﻿namespace Mediator;
+
+//public abstract class ChatRoom
+//{
+//    public abstract void Register(TeamMember teamMember);
+//    public abstract void Send(string from, string message);
+//}
+
+/// <summary>
+/// Mediator
+/// </summary>
+public interface IChatRoomMediator
+{
+    void Register(TeamMember teamMember);
+    void Send(string from, string message);
+    void Send(string from, string to, string message);
+    void SendTo<T>(string from, string message) where T : TeamMember;
+}
+
+/// <summary>
+/// Colleague
+/// </summary>
+public abstract class TeamMember
+{
+    // team member chat with each ither using this mediator
+    private IChatRoomMediator? _chatroom;
+
+    public TeamMember(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+
+    internal void SetChatroom(IChatRoomMediator chatRoom)
+    {
+        _chatroom = chatRoom;
+    }
+
+    public void Send(string to, string message)
+    {
+        _chatroom?.Send(Name, to, message);
+    }
+
+    public void SendTo<T>(string message) where T : TeamMember
+    {
+        _chatroom?.SendTo<T>(Name, message);
+    }
+
+    public void Send(string message)
+    {
+        _chatroom?.Send(Name, message);
+    }
+
+    public virtual void Receive(string from, string message)
+    {
+        Console.WriteLine($"Message {from} to {Name}: {message}");
+    }
+}
+
+/// <summary>
+/// ConcreteColleague
+/// </summary>
+public class Lawyer : TeamMember
+{
+    public Lawyer(string name) : base(name)
+    {
+    }
+
+    public override void Receive(string from, string message)
+    {
+        Console.WriteLine($"{nameof(Lawyer)} {Name} received: ");
+        base.Receive(from, message);
+    }
+}
+
+/// <summary>
+/// ConcreteColleague
+/// </summary>
+public class AccountManager : TeamMember
+{
+    public AccountManager(string name) : base(name)
+    {
+    }
+
+    public override void Receive(string from, string message)
+    {
+        Console.WriteLine($"{nameof(AccountManager)} {Name} received: ");
+        base.Receive(from, message);
+    }
+}
+
+
+/// <summary>
+/// ConcreteMediator
+/// </summary>
+public class TeamChatRoomMediator : IChatRoomMediator
+{
+    private readonly Dictionary<string, TeamMember> _teamMembers = [];
+
+    public void Register(TeamMember teamMember)
+    {
+        teamMember.SetChatroom(this);
+
+        if (!_teamMembers.ContainsKey(teamMember.Name))
+        {
+            _teamMembers.Add(teamMember.Name, teamMember);
+        }
+    }
+
+    public void Send(string from, string message)
+    {
+        foreach (var teamMember in _teamMembers.Values)
+        {
+            teamMember.Receive(from, message);
+        }
+    }
+
+    public void Send(string from, string to, string message)
+    {
+        var teamMember = _teamMembers[to];
+        teamMember?.Receive(from, message);
+    }
+
+    public void SendTo<T>(string from, string message) where T : TeamMember
+    {
+        foreach (var teamMember in _teamMembers.Values.OfType<T>())
+        {
+            teamMember.Receive(from, message);
+        }
+    }
+}
---
+++ Patterns/03.Behavioral/05 - Mediator/Mediator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Mediator;
+
+Console.Title = "Mediator";
+
+// Team members
+var sven = new Lawyer("Sven");
+var kenneth = new Lawyer("Kenneth");
+var ann = new AccountManager("Ann");
+var john = new AccountManager("John");
+var kylie = new AccountManager("Kylie");
+
+// Chat Room Mediator
+TeamChatRoomMediator teamChatroomMediator = new();
+teamChatroomMediator.Register(sven);
+teamChatroomMediator.Register(kenneth);
+teamChatroomMediator.Register(ann);
+teamChatroomMediator.Register(john);
+teamChatroomMediator.Register(kylie);
+
+// Interaction in chat room via mediator
+ann.Send("Hi everyone, can someone have a look at file ABC123?  I need a compliance check.");
+sven.Send("On it!");
+sven.Send("Ann", "Could you join me in a Teams call?");
+sven.Send("Ann", "All good :)");
+ann.SendTo<AccountManager>("The file was approved!");
+
+Console.ReadKey();
