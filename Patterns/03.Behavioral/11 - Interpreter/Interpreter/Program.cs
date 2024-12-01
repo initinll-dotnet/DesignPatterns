5bd84f4230270ec62e3155065e6370ead88810b8
initinll <initinli@yahoo.com>
2024-12-01 19:29:42 +0530

    added Iterator pattern concept

---
+++ Patterns/03.Behavioral/11 - Interpreter/Interpreter/Program.cs
@@ -0,0 +1,36 @@
+﻿using Interpreter;
+
+Console.Title = "Interpreter";
+
+List<RomanExpression> expressions =
+[
+    new RomanHunderdExpression(),
+    new RomanTenExpression(),
+    new RomanOneExpression(),
+];
+
+var context = new RomanContext(input: 5);
+
+foreach (var expression in expressions)
+{
+    expression.Interpret(context);
+}
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: 5 = {context.Output}");
+
+context = new RomanContext(input: 81);
+
+foreach (var expression in expressions)
+{
+    expression.Interpret(context);
+}
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: 81 = {context.Output}");
+
+context = new RomanContext(input: 733);
+
+foreach (var expression in expressions)
+{
+    expression.Interpret(context);
+}
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: 733 = {context.Output}");
+
+Console.ReadKey();
