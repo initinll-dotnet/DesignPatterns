7f8d0aa331c40ebfa01db5cba7800256f2f6a226
initinll <initinli@yahoo.com>
2024-12-01 18:36:17 +0530

    added Iterator pattern concept

---
+++ Patterns/03.Behavioral/09 - Iterator/Iterator/Program.cs
@@ -0,0 +1,25 @@
+﻿using Iterator;
+
+Console.Title = "Iterator";
+
+// create the collection
+PeopleCollection people =
+[
+    new Person("Kevin Dockx", "Belgium"),
+    new Person("Gill Cleeren", "Belgium"),
+    new Person("Roland Guijt", "The Netherlands"),
+    new Person("Thomas Claudius Huber", "Germany"),
+];
+
+// create the iterator
+var peopleIterator = people.CreateIterator();
+
+// use the iterator to run through the people
+// in the collection; they should come out 
+// in alphabetical order
+for (Person person = peopleIterator.First();!peopleIterator.IsDone;person = peopleIterator.Next())
+{
+    Console.WriteLine(person?.Name);
+}
+
+Console.ReadKey();
